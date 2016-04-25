02c5903b619d166ea11675c71f639723259e6e84
Ron Belmont <belmonrj@gmail.com>
2016-04-25 15:49:47 -0400

    Updated tilemapper_RB to do xbin and ybin, updated a few variable names, etc

--- tilemapper_RB.C
+++ tilemapper_RB.C
@@ -1,37 +1,42 @@
 #include "TString.h"
 #include <iostream>
 
-int dotilemapper(TString);
+//int dotilemapper(TString);
+int dotilemapper(TString, int, int);
 
-ofstream mpv("mpv.txt");
+ofstream fout("mpv.txt");
 
 void tilemapper_RB()
 {
 
-  dotilemapper("data/beam_00002353-0000.root"); // test position all 8 tiles (one appears to be disconnected/off)
-  dotilemapper("data/beam_00002354-0000.root");
-  dotilemapper("data/beam_00002355-0000.root");
-  dotilemapper("data/beam_00002357-0000.root");
-  dotilemapper("data/beam_00002358-0000.root");
-  dotilemapper("data/beam_00002359-0000.root");
-  dotilemapper("data/beam_00002361-0000.root");
-  dotilemapper("data/beam_00002363-0000.root");
-  dotilemapper("data/beam_00002364-0000.root");
-  dotilemapper("data/beam_00002365-0000.root");
-  dotilemapper("data/beam_00002366-0000.root");
-  dotilemapper("data/beam_00002367-0000.root");
-  dotilemapper("data/beam_00002368-0000.root");
-  dotilemapper("data/beam_00002369-0000.root");
-  dotilemapper("data/beam_00002370-0000.root");
-  dotilemapper("data/beam_00002371-0000.root");
-  dotilemapper("data/beam_00002372-0000.root");
-  dotilemapper("data/beam_00002374-0000.root");
-  dotilemapper("data/beam_00002375-0000.root");
-  
+  dotilemapper("data/beam_00002353-0000.root",1,0); // 2353 976 180  1 0
+  dotilemapper("data/beam_00002354-0000.root",0,0); // 2354 1076 180 0 0
+  dotilemapper("data/beam_00002355-0000.root",2,0); // 2355 690 180  2 0
+  dotilemapper("data/beam_00002356-0000.root",3,0); // 2356 410 180  3 0
+  dotilemapper("data/beam_00002357-0000.root",4,0); // 2357 215 180  4 0
+  dotilemapper("data/beam_00002358-0000.root",4,1); // 2358 215 100  4 1
+  dotilemapper("data/beam_00002359-0000.root",3,1); // 2359 410 100  3 1
+  dotilemapper("data/beam_00002361-0000.root",2,1); // 2361 690 100  2 1
+  dotilemapper("data/beam_00002362-0000.root",1,1); // 2362 976 100  1 1
+  dotilemapper("data/beam_00002363-0000.root",0,1); // 2363 1076 100 0 1
+  dotilemapper("data/beam_00002364-0000.root",0,1); // 2364 1056 167 0 1
+  dotilemapper("data/beam_00002366-0000.root",1,1); // 2366 1016 167 1 1
+  dotilemapper("data/beam_00002367-0000.root",2,1); // 2367 976 167  2 1
+  dotilemapper("data/beam_00002368-0000.root",3,1); // 2368 936 167  3 1
+  dotilemapper("data/beam_00002369-0000.root",4,1); // 2369 896 167  4 1
+  dotilemapper("data/beam_00002370-0000.root",4,0); // 2370 896 209  4 0
+  dotilemapper("data/beam_00002371-0000.root",3,0); // 2371 936 209  3 0
+  dotilemapper("data/beam_00002373-0000.root",2,0); // 2373 976 209  2 0
+  dotilemapper("data/beam_00002374-0000.root",1,0); // 2374 1016 209 1 0
+  dotilemapper("data/beam_00002375-0000.root",0,0); // 2375 1056 209 0 0
+
+
 }
 
-int dotilemapper( TString filename )
+int dotilemapper ( TString filename, int xbin, int ybin )
 {
+
+  fout << xbin << " " << ybin << " ";
 
   gROOT->SetStyle("Modern");
   gStyle->SetOptFit(111);
@@ -47,8 +52,8 @@ int dotilemapper( TString filename )
   TString outfilename = "output/tilemapper_";
   outfilename += gSystem->BaseName( filename );
 
-  TFile *fout = new TFile(outfilename,"recreate");
-  fout->cd();
+  TFile *foutput = new TFile(outfilename,"recreate");
+  foutput->cd();
 
   TString adc;
   TString name;
@@ -98,9 +103,9 @@ int dotilemapper( TString filename )
     W->Fit("flandau",drawstring,cutstring);
     float mean = flandau->GetParameter(1);
     cout << "the mean is " << mean << endl;
-    mpv << mean << " ";
+    fout << mean << " ";
   }
-  mpv<<endl;
+  fout<<endl;
 
   TString pdffilename = "output/tilemapper_";
   pdffilename += gSystem->BaseName( filename );
@@ -109,8 +114,10 @@ int dotilemapper( TString filename )
   pdffilename.ReplaceAll(".pdf",".png");
   c1->Print(pdffilename,"png Portrait");
 
-  fout->Write();
-  fout->Close();
-  delete fout;
+  foutput->Write();
+  foutput->Close();
+  delete foutput;
+
+  return 0; // this used to have a return value...
 
 }
