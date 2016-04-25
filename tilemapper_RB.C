8e73faa550f893d9ebe036e6b734d4eb3dd4d121
Ron Belmont <belmonrj@gmail.com>
2016-04-25 12:10:03 -0400

    Some changes done with Sebastian

--- tilemapper_RB.C
+++ tilemapper_RB.C
@@ -3,11 +3,31 @@
 
 int dotilemapper(TString);
 
+ofstream mpv("mpv.txt");
+
 void tilemapper_RB()
 {
 
-  dotilemapper("beam_00002353-0000.root"); // test position all 8 tiles (one appears to be disconnected/off)
-
+  dotilemapper("data/beam_00002353-0000.root"); // test position all 8 tiles (one appears to be disconnected/off)
+  dotilemapper("data/beam_00002354-0000.root");
+  dotilemapper("data/beam_00002355-0000.root");
+  dotilemapper("data/beam_00002357-0000.root");
+  dotilemapper("data/beam_00002358-0000.root");
+  dotilemapper("data/beam_00002359-0000.root");
+  dotilemapper("data/beam_00002361-0000.root");
+  dotilemapper("data/beam_00002363-0000.root");
+  dotilemapper("data/beam_00002364-0000.root");
+  dotilemapper("data/beam_00002365-0000.root");
+  dotilemapper("data/beam_00002366-0000.root");
+  dotilemapper("data/beam_00002367-0000.root");
+  dotilemapper("data/beam_00002368-0000.root");
+  dotilemapper("data/beam_00002369-0000.root");
+  dotilemapper("data/beam_00002370-0000.root");
+  dotilemapper("data/beam_00002371-0000.root");
+  dotilemapper("data/beam_00002372-0000.root");
+  dotilemapper("data/beam_00002374-0000.root");
+  dotilemapper("data/beam_00002375-0000.root");
+  
 }
 
 int dotilemapper( TString filename )
@@ -21,6 +41,14 @@ int dotilemapper( TString filename )
   TCanvas *c1 = new TCanvas("c1","Tiletester high gain",1280,960);
   c1->Divide(4,2);
   //  c1->Divide(2,2);
+
+  TTree *W = (TTree *)f->Get("W");
+
+  TString outfilename = "output/tilemapper_";
+  outfilename += gSystem->BaseName( filename );
+
+  TFile *fout = new TFile(outfilename,"recreate");
+  fout->cd();
 
   TString adc;
   TString name;
@@ -39,6 +67,8 @@ int dotilemapper( TString filename )
     cout << name << " " << adc << endl;
     tower_number++;
   }
+
+  TF1 *flandau = new TF1("flandau","landau",0,500);
 
   TString drawstring;
   TString cutstring;
@@ -64,16 +94,23 @@ int dotilemapper( TString filename )
 
     c1->cd(ican++);
     //    W->Draw(drawstring,cutstring);
-    W->Fit("landau",drawstring,cutstring);
-
+    //W->Fit("landau",drawstring,cutstring);
+    W->Fit("flandau",drawstring,cutstring);
+    float mean = flandau->GetParameter(1);
+    cout << "the mean is " << mean << endl;
+    mpv << mean << " ";
   }
+  mpv<<endl;
 
-  TSystem *sys = new TSystem();
-  TString pdffilename = "tilemapper_";
-  pdffilename += sys->BaseName( filename );
+  TString pdffilename = "output/tilemapper_";
+  pdffilename += gSystem->BaseName( filename );
   pdffilename.ReplaceAll(".root",".pdf");
   c1->Print(pdffilename,"pdf Portrait");
   pdffilename.ReplaceAll(".pdf",".png");
   c1->Print(pdffilename,"png Portrait");
+
+  fout->Write();
+  fout->Close();
+  delete fout;
 
 }
