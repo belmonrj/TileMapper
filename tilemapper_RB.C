dc9c0b43ecafeeab5233006b0daea2fe4b5d5521
Ron Belmont <belmonrj@gmail.com>
2016-05-09 14:15:21 -0400

    Added 16 GeV pion runs to tilemapper_RB

--- tilemapper_RB.C
+++ tilemapper_RB.C
@@ -9,27 +9,50 @@ ofstream fout("mpv.txt");
 void tilemapper_RB()
 {
 
-  dotilemapper("data/beam_00002353-0000.root",1,0); // 2353 976 180  1 0
-  dotilemapper("data/beam_00002354-0000.root",0,0); // 2354 1076 180 0 0
-  dotilemapper("data/beam_00002355-0000.root",2,0); // 2355 690 180  2 0
-  dotilemapper("data/beam_00002356-0000.root",3,0); // 2356 410 180  3 0
-  dotilemapper("data/beam_00002357-0000.root",4,0); // 2357 215 180  4 0
-  dotilemapper("data/beam_00002358-0000.root",4,1); // 2358 215 100  4 1
-  dotilemapper("data/beam_00002359-0000.root",3,1); // 2359 410 100  3 1
-  dotilemapper("data/beam_00002361-0000.root",2,1); // 2361 690 100  2 1
-  dotilemapper("data/beam_00002362-0000.root",1,1); // 2362 976 100  1 1
-  dotilemapper("data/beam_00002363-0000.root",0,1); // 2363 1076 100 0 1
-  dotilemapper("data/beam_00002364-0000.root",0,1); // 2364 1056 167 0 1
-  dotilemapper("data/beam_00002366-0000.root",1,1); // 2366 1016 167 1 1
-  dotilemapper("data/beam_00002367-0000.root",2,1); // 2367 976 167  2 1
-  dotilemapper("data/beam_00002368-0000.root",3,1); // 2368 936 167  3 1
-  dotilemapper("data/beam_00002369-0000.root",4,1); // 2369 896 167  4 1
-  dotilemapper("data/beam_00002370-0000.root",4,0); // 2370 896 209  4 0
-  dotilemapper("data/beam_00002371-0000.root",3,0); // 2371 936 209  3 0
-  dotilemapper("data/beam_00002373-0000.root",2,0); // 2373 976 209  2 0
-  dotilemapper("data/beam_00002374-0000.root",1,0); // 2374 1016 209 1 0
-  dotilemapper("data/beam_00002375-0000.root",0,0); // 2375 1056 209 0 0
+  // dotilemapper("data/beam_00002353-0000.root",1,0); // 2353 976 180  1 0
+  // dotilemapper("data/beam_00002354-0000.root",0,0); // 2354 1076 180 0 0
+  // dotilemapper("data/beam_00002355-0000.root",2,0); // 2355 690 180  2 0
+  // dotilemapper("data/beam_00002356-0000.root",3,0); // 2356 410 180  3 0
+  // dotilemapper("data/beam_00002357-0000.root",4,0); // 2357 215 180  4 0
+  // dotilemapper("data/beam_00002358-0000.root",4,1); // 2358 215 100  4 1
+  // dotilemapper("data/beam_00002359-0000.root",3,1); // 2359 410 100  3 1
+  // dotilemapper("data/beam_00002361-0000.root",2,1); // 2361 690 100  2 1
+  // dotilemapper("data/beam_00002362-0000.root",1,1); // 2362 976 100  1 1
+  // dotilemapper("data/beam_00002363-0000.root",0,1); // 2363 1076 100 0 1
+  // dotilemapper("data/beam_00002364-0000.root",0,1); // 2364 1056 167 0 1
+  // dotilemapper("data/beam_00002366-0000.root",1,1); // 2366 1016 167 1 1
+  // dotilemapper("data/beam_00002367-0000.root",2,1); // 2367 976 167  2 1
+  // dotilemapper("data/beam_00002368-0000.root",3,1); // 2368 936 167  3 1
+  // dotilemapper("data/beam_00002369-0000.root",4,1); // 2369 896 167  4 1
+  // dotilemapper("data/beam_00002370-0000.root",4,0); // 2370 896 209  4 0
+  // dotilemapper("data/beam_00002371-0000.root",3,0); // 2371 936 209  3 0
+  // dotilemapper("data/beam_00002373-0000.root",2,0); // 2373 976 209  2 0
+  // dotilemapper("data/beam_00002374-0000.root",1,0); // 2374 1016 209 1 0
+  // dotilemapper("data/beam_00002375-0000.root",0,0); // 2375 1056 209 0 0
 
+  dotilemapper("data/beam_00002669-0000.root",1,0); // 2669  976 180  7268
+  dotilemapper("data/beam_00002670-0000.root",0,1); // 2670 1056 167  10981
+  dotilemapper("data/beam_00002671-0000.root",1,1); // 2671 1016 167  11060
+  dotilemapper("data/beam_00002672-0000.root",2,1); // 2672  976 167  11060
+  dotilemapper("data/beam_00002673-0000.root",3,1); // 2673  936 167  11139
+  dotilemapper("data/beam_00002674-0000.root",4,1); // 2674  896 167  14694
+  dotilemapper("data/beam_00002675-0000.root",1,0); // 2675  976 180  10349
+  dotilemapper("data/beam_00002676-0000.root",0,0); // 2676 1076 180  10902
+  dotilemapper("data/beam_00002677-0000.root",2,0); // 2677  690 180  7979
+  dotilemapper("data/beam_00002678-0000.root",2,0); // 2678  690 180  10823
+  dotilemapper("data/beam_00002679-0000.root",3,0); // 2679  410 180  11060
+  dotilemapper("data/beam_00002680-0000.root",4,0); // 2680  215 180  10902
+  dotilemapper("data/beam_00002681-0000.root",4,0); // 2681  896 209  10981
+  dotilemapper("data/beam_00002682-0000.root",3,0); // 2682  936 209  11534
+  dotilemapper("data/beam_00002683-0000.root",2,0); // 2683  976 209  11060
+  dotilemapper("data/beam_00002684-0000.root",1,0); // 2684 1016 209  10981
+  dotilemapper("data/beam_00002685-0000.root",0,0); // 2685 1056 209  10981
+  dotilemapper("data/beam_00002686-0000.root",0,1); // 2686 1076 100  10981
+  dotilemapper("data/beam_00002687-0000.root",1,1); // 2687  976 100  10981
+  dotilemapper("data/beam_00002688-0000.root",9,1); // 2688  770 100  11139
+  dotilemapper("data/beam_00002689-0000.root",2,1); // 2689  690 100  11613
+  dotilemapper("data/beam_00002690-0000.root",3,1); // 2690  410 100  11139
+  dotilemapper("data/beam_00002691-0000.root",4,1); // 2691  215 100  11060
 
 }
 
