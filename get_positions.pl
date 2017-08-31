91572d020cb7cd0aac3656b91e9045e626d959b3
Ron Belmont <belmonrj@gmail.com>
2017-08-31 14:05:42 -0400

    This helps with the output format

--- get_positions.pl
+++ get_positions.pl
@@ -7,11 +7,13 @@ for ( $i = 3750; $i < 3900; $i++ )
     $command = "ddump -p 910 -t 9 JinData/beam/beam_0000$i-0000.prdf | grep \'E:2CH\' | awk \'{print \$3}\' ";
     $h = `$command`;
     chomp($h);
+    if ( !$h ) { $h = -9; }
     print $h;
     print " ";
     $command = "ddump -p 910 -t 9 JinData/beam/beam_0000$i-0000.prdf | grep \'E:2CV\' | awk \'{print \$3}\' ";
     $h = `$command`;
     chomp($h);
+    if ( !$h ) { $h = -9; }
     print $h;
     print "\n";
 }
